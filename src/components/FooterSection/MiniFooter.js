c384dd8c2fb38cf4f20a04d60f5f8c62f1f2ecb5
Carlstong <carlstongrefalde@gmail.com>
2020-12-14 23:37:18 +0800

    errir fixed

--- src/components/FooterSection/MiniFooter.js
+++ src/components/FooterSection/MiniFooter.js
@@ -9,7 +9,7 @@ export default function MiniFooter() {
         <div className="row mt-5 mb-5">
             <div className="col col-md-6 text-light">© 2020 Optomeye Eyeswear | All rights reserved
 </div>
-            <div className="col col-md-6 text-light"><a href="#" className="text-light">Back to Top</a></div>
+            <div className="col col-md-6 text-light"><a href="home" className="text-light">Back to Top</a></div>
 
         </div>
 
